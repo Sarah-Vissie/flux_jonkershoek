7ada3ac734d73196f82caaa914ec1b593ad5cda6
Res <res.altwegg@gmail.com>
2025-07-25 13:44:27 +0200

    aded some plots

--- 03_validation.R
+++ 03_validation.R
@@ -3,7 +3,7 @@
 load("jagsoutput_temp_light.Rdata")
 
 # Extract CI from the output matrix from jags.out
-summary(jags.out)
+# summary(jags.out)
 out.matrix <- as.matrix(jags.out)
 out.matrix[,1:5] # first 5 colums stores b1, b2, rho, tau_add, tau_obs, 3000 rows
 
@@ -22,17 +22,24 @@ daytime <- c("05","06","07","08","09","10","11","12","13","14","15","16","17","1
 nighttime <- c("21","22","23","00","01","02","03","04")
 
 modelcheckingplots <- function(out){
-  par(mfrow=c(2,2))
+  par(mfrow=c(2,3))
   out1 <- out
   out <- out[out$hour%in%daytime,]
-  plot(out$Fc_molar[out$hour%in%daytime], out$`50%`, ylim=c(min(out$`2.5%`), max(max(out$`97.5%`))))
+  plot(out$Fc_molar, out$`50%`, ylim=c(min(out$`2.5%`), max(max(out$`97.5%`))))
   abline(a=0,b=1)
   for (i in 1:dim(out)[1]) {segments(x0=out$Fc_molar[i],y0=out$`2.5%`[i],y1=out$`97.5%`[i])}
+
+  plot(out$Tc_Avg, out$`50%`- out$Fc_molar, ylab="Predicted - observed", xlab = "Temperature")
+  plot(out$Rn, out$`50%`- out$Fc_molar, ylab="Predicted - observed", xlab = "Light")
 
   out <- out1[out1$hour%in%nighttime,]
   plot(out$Fc_molar, out$`50%`, ylim=c(min(out$`2.5%`), max(max(out$`97.5%`))))
   abline(a=0,b=1)
   for (i in 1:dim(out)[1]) {segments(x0=out$Fc_molar[i],y0=out$`2.5%`[i],y1=out$`97.5%`[i])}
+
+  plot(out$Tc_Avg, out$`50%`- out$Fc_molar, ylab="Predicted - observed", xlab = "Temperature")
+  plot(out$Rn, out$`50%`- out$Fc_molar, ylab="Predicted - observed", xlab = "Light")
+
   }
 
 
